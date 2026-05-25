f12ec5dec57ce9c3ba073c69f4a672b09024acc0
bongang101101 <43650138+bongang101101@users.noreply.github.com>
2026-05-25 20:40:23 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,10 @@
+<script>
+  atOptions = {
+    'key' : '981e07dcb74077485d22c2a39b017e72',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+</script>
+<script src="https://contendbugsexistence.com/981e07dcb74077485d22c2a39b017e72/invoke.js"></script>
